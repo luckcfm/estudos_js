937874349e7ca1a8abbf96be0f36ce7763f0438b
Lucas Medeiros <luckcfm@gmail.com>
2018-04-24 01:17:07 -0300

    Gulp configurado para fazer o transpiling

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 //Gulp dependecies go here
 
 gulp.task('default', function(){
     //gulp tasks go hhere
+    //Node source
+    gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulpp.dest("dist"));
+
+    //browser source
+    gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
 });
